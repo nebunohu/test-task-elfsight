b110bc007363be7ec20052f08128b9fb92756625
Barashkov <nebunohu@gmail.com>
2022-02-18 17:30:05 +0300

    Intermediate commit

--- src/components/characters-list-container/characters-list-container.tsx
+++ src/components/characters-list-container/characters-list-container.tsx
@@ -1,6 +1,5 @@
 import { 
-  OutlinedInput, 
-  InputLabel,
+  OutlinedInput,
   Select, 
   MenuItem, 
   Checkbox, 
@@ -16,20 +15,24 @@ import CharacterCard from "../character-card/character-card";
 // Styles 
 import styles from './characters-list-container.module.css';
 
-type TFilterNames = 'name';
-
 type TFilters = {
   name: string;
+  statuses: Array<string>;
+  specieses: Array<string>;
+  genders: Array<string>;
   types: Array<string>;
 }
 
 const CharactersListContainer: FC = () => {
   const [rerender, setRerender] = useState(false);
-  const [renderFiltered, setRenderFiltered] = useState(false);
   const { list, getCharactersRequest } = useSelector(store => store.characters);
-  const [filteredCharacters, setFilteredCharacters] = useState(list);
-  const [selectedCharactersTypes, setSelectedCharactersTypes] = useState<Array<string>>([]);
-  const [filters, setFilters] = useState<TFilters>({name: '', types: []});
+  const [filters, setFilters] = useState<TFilters>({
+    name: '', 
+    types: [],
+    statuses: [],
+    specieses: [],
+    genders: [],
+  });
   const { filteredList } = useSelector(store => store.characters);
   const dispatch = useDispatch();
   
@@ -40,51 +43,96 @@ const CharactersListContainer: FC = () => {
     setFilters({...filters, name: nameFilterValue});
   }
 
-  const handleTypesFilterChange = (event: SelectChangeEvent<typeof selectedCharactersTypes>) => {
+  const handleTypesFilterChange = (event: SelectChangeEvent<Array<string>>, prop: keyof TFilters) => {
     const {
       target: { value },
     } = event;
     setFilters({
       ...filters,
-      types: typeof value === 'string' ? value.split(',') : value,
+      [prop]: typeof value === 'string' ? value.split(',') : value,
     });
   }
 
-  const getListOfTypes = (list: Array<TCharacter>): Set<string> => {
-    let listOfTypes: Set<string> = new Set();
+  const getListOfPropValues = (list: Array<TCharacter>, prop: keyof TCharacter): Array<string> => {
+    let listOfPropValues: Set<string> = new Set();
     if(list.length) {
-      list.forEach(el => listOfTypes.add(el.type));
+      list.forEach(el => listOfPropValues.add(el[prop] as string));
     }
 
-    return listOfTypes; 
+    return Array.from(listOfPropValues); 
   }
-  const characterTypes = useRef(getListOfTypes(filteredList));
-  
+
+  const filterList = (key: keyof typeof filters, filteringList: Array<TCharacter>) => {
+    //const filteredListAtStart = filteredCharactersList;
+    let filteredList: Array<TCharacter> = [];
+    const temp = filters[key] as string[];
+    if(temp.length) {
+      temp.forEach(filterEl => {
+        filteredList = filteredList.concat(filteringList.filter(el => el.type === filterEl));
+      })
+      //filteredCharactersList = filteredListByType;
+    }
+    return filteredList;
+  }
+
+
+  const characterTypes = useRef(getListOfPropValues(filteredList, 'type'));
+  if (!characterTypes.current.length) {
+    characterTypes.current = getListOfPropValues(filteredList, 'type'); // ?
+  }
+  const characterStatuses = useRef(getListOfPropValues(filteredList, 'status'));
+  if (!characterStatuses.current.length) {
+    characterStatuses.current = getListOfPropValues(filteredList, 'status'); // ?
+  }
 
   useEffect(() => {
     let nameFilteredCharactersList = [...list];
+    let filteredCharactersList = [...list];
     let typesFilteredList: Array<TCharacter> = [];
     let key: keyof typeof filters;
     for (key in filters) {
       if( key === 'name' ) {
         const temp = filters[key];
-        nameFilteredCharactersList = nameFilteredCharactersList.filter(el => el.name.toLowerCase().includes(temp.toLowerCase()));
-        characterTypes.current = getListOfTypes(nameFilteredCharactersList);
-      }
+        filteredCharactersList = filteredCharactersList.filter(el => el.name.toLowerCase().includes(temp.toLowerCase()));
+      } 
       if( key === 'types' ) {
+        filteredCharactersList = filterList(key, filteredCharactersList);}
+        /*const filteredListAtStart = filteredCharactersList;
+        let filteredListByType: Array<TCharacter> = [];
         const temp = filters[key];
-        temp.forEach(filterEl => {
-          typesFilteredList = typesFilteredList.concat(nameFilteredCharactersList.filter(el => el.type === filterEl));
-        })
+        if(temp.length) {
+          temp.forEach(filterEl => {
+            filteredListByType = filteredListByType.concat(filteredListAtStart.filter(el => el.type === filterEl));
+          })
+          filteredCharactersList = filteredListByType;
+        }*/
+      //}
+      if( key === 'statuses' ) {
         
+        const filteredListAtStart = filteredCharactersList;
+        let filteredListByStatus: Array<TCharacter> = [];
+        const temp = filters[key];
+        if(temp.length) {
+          temp.forEach(filterEl => {
+            filteredListByStatus = filteredListByStatus.concat(filteredListAtStart.filter(el => el.status === filterEl));
+          })
+          filteredCharactersList = filteredListByStatus;
+        }
       }
+      //if( key === 'specieses' ) {
+
+      //}
+      //if( key === 'genders' ) {
+
+      //}
     }
-    dispatch(setFilteredCharactersList(filters.types.length ? typesFilteredList : nameFilteredCharactersList));
-  },[filters, list]);
+    //characterTypes.current = getListOfPropValues(filteredCharactersList, 'type');
+    //characterStatuses.current = getListOfPropValues(filteredCharactersList, 'status');
+    dispatch(setFilteredCharactersList(filteredCharactersList));
+  },[filters, list, dispatch]);
 
 
   if(rerender) setRerender(false);
-  //const renderList = renderFiltered ? filteredCharacters : list;
 
   return (
     <div className={`${styles.componentWrapper}`}>
@@ -110,13 +158,31 @@ const CharactersListContainer: FC = () => {
           value={filters.types}
           input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
           renderValue={(selected) => selected.join(', ')}
-          onChange={handleTypesFilterChange}
+          onChange={(event) => handleTypesFilterChange(event, 'types')}
         >
           {Array.from(characterTypes.current).map((el,index) => <MenuItem value={el} key={index}>
             <Checkbox checked={filters.types.indexOf(el) > -1} />
             <ListItemText primary={el} />
           </MenuItem>)}
         </Select>
+        <label htmlFor="type">Filter by status</label>
+        <Select
+          className={`${styles.selectRoot}`}
+          multiple
+          id='status'
+          labelId='status'
+          size='small'
+          value={filters.statuses}
+          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+          renderValue={(selected) => selected.join(', ')}
+          onChange={(event) => handleTypesFilterChange(event, 'statuses')}
+        >
+          {Array.from(characterStatuses.current).map((el,index) => <MenuItem value={el} key={index}>
+            <Checkbox checked={filters.statuses.indexOf(el) > -1} />
+            <ListItemText primary={el} />
+          </MenuItem>)}
+        </Select>
+        
       </form>
       <div className={`${styles.charactersListWrapper}`}>
         {getCharactersRequest ? "Loading... " : filteredList.map((el: any, index: number) => <CharacterCard character={el} key={index} />)}
